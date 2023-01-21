3f470b6ddf05c14f12f77bf517db5330bf29e566
nostalgiccat <nesdom27@gmail.com>
2023-01-21 09:04:08 -0700

    Initial commit

---
+++ src/robots.js
@@ -0,0 +1,28 @@
+export const robots = [
+    {
+        id: 1, 
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 2, 
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email: 'Shanna@melissa.tv'
+    },
+    {
+        id: 3, 
+        name: 'Clemen Baunch',
+        username: 'Sam',
+        email: 'Snathan@yez.net'
+    },
+    {
+        id: 4, 
+        name: 'Whoever Tf',
+        username: 'Spider',
+        email: 'glassesarecool@gmail.com'
+    }    
+    
+]
+
