d4f81346857545a880909b7eb1889a458efd522a
nostalgiccat <nesdom27@gmail.com>
2023-01-22 10:45:56 -0700

    adding styling to the page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from './CardList';
 import {robots} from './robots';
 import SearchBox from './SearchBox';
+import './App.css'; 
 
 
 class App extends Component  {
@@ -16,13 +17,14 @@ class App extends Component  {
     onSearchChange = (event) => {
         this.setState({ searchfield: event.target.value})
     }
+    
     render() {
         const filteredRobots = this.state.robots.filter(robot =>{
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
         return (
             <div className="tc ">
-                <h1>Robofriends</h1>
+                <h1 className="f1">Robofriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <CardList robots={filteredRobots}/>
             </div>
