4226e13aef7466eecc33f3d8abe853c69acaf6b5
nostalgiccat <nesdom27@gmail.com>
2023-01-23 17:24:08 -0700

    build version

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'; 
-import CardList from './CardList';
-import SearchBox from './SearchBox';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
 import './App.css'; 
-import Scroll from './Scroll'; 
+import Scroll from '../components/Scroll'; 
 
 
 class App extends Component  {
@@ -26,13 +26,13 @@ class App extends Component  {
     }
     
     render() {
-        const filteredRobots = this.state.robots.filter(robot =>{
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot =>{
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
-        if (this.state.robots.length === 0) {
-            return <h1>Loading</h1>
-        } else {
-            return (
+        return !robots.length ?    // Ternary operator 
+            <h1>Loading</h1>:
+            (
                 <div className="tc ">
                     <h1 className="f1">Robofriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
@@ -42,10 +42,10 @@ class App extends Component  {
                 </div>
                 
             );
-        }
-        
     }
-    
+        
 }
+    
+
 
 export default App; 
